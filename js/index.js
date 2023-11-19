864449ba96de7c145301294f0a79d2bbbffc274e
allygraphy <81565182+allygraphy@users.noreply.github.com>
2023-11-19 21:43:05 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 window.onload = function(){
-    requestCoupon();
+    couponInit();
+    // requestCoupon();
 }
 
 const couponInit = (getData) => {
@@ -7,18 +8,21 @@ const couponInit = (getData) => {
     [...couponElems].forEach((coupon, index) => {
         const couponNumber = index + 1;
         const reactElem = coupon.parentNode;
-        const targetData = getData.filter(targetCoupon => targetCoupon.cupon == couponNumber);
-        if(targetData.length > 0){
-            targetData[0].state == 0 ? reactElem.classList.remove("on") : reactElem.classList.add("on")
-        }
+        // const targetData = getData.filter(targetCoupon => targetCoupon.cupon == couponNumber);
+        // if(targetData.length > 0){
+        //     targetData[0].state == 0 ? reactElem.classList.remove("on") : reactElem.classList.add("on")
+        // }
+        // coupon.onclick = () => {
+        //     !reactElem.classList.contains("on") && modalOpen(reactElem, targetData)
+        // }
         coupon.onclick = () => {
-            !reactElem.classList.contains("on") && modalOpen(reactElem, targetData)
+            !reactElem.classList.contains("on") && modalOpen(reactElem)
         }
     })
 }
 
 
-const modalOpen = (targetCoupon, targetData) => {
+const modalOpen = (targetCoupon) => {
     const modal = document.querySelector(".modal_wrap");
     modal.style.display = "flex";
 
@@ -27,7 +31,7 @@ const modalOpen = (targetCoupon, targetData) => {
     okBtn.onclick = () => {
         !targetCoupon.classList.contains("on") && targetCoupon.classList.add("on");
         modal.style.display = "none";
-        requestChangeState(targetData[0]);
+        // requestChangeState(targetData[0]);
     }
 
     closeBtn.onclick = () => {
@@ -35,37 +39,37 @@ const modalOpen = (targetCoupon, targetData) => {
     }
 }
 
-const requestChangeState = (targetData) => {
-    const responseChangeState = result => {}
-    fetch("https://news.glserver.co.kr/",{
-        method : "POST",
-        body : JSON.stringify({
-            ctl : "Product",
-            param1 : "webTestChange",
-            state : targetData.state == 1 ? 0 : 1,
-            cupon : targetData.cupon,
-        })
-    })
-    .then(response => response.text())
-    .then(result => responseChangeState(result))
-    .catch(error => console.log("error : " + error));
-}
+// const requestChangeState = (targetData) => {
+//     const responseChangeState = result => {}
+//     fetch("https://news.glserver.co.kr/",{
+//         method : "POST",
+//         body : JSON.stringify({
+//             ctl : "Product",
+//             param1 : "webTestChange",
+//             state : targetData.state == 1 ? 0 : 1,
+//             cupon : targetData.cupon,
+//         })
+//     })
+//     .then(response => response.text())
+//     .then(result => responseChangeState(result))
+//     .catch(error => console.log("error : " + error));
+// }
 
-const requestCoupon = () => {
-    const responseCoupon = result => {
-        result = JSON.parse(result);
-        if(result.result == 1){
-            couponInit(result.value);
-        }
-    }
-    fetch("https://news.glserver.co.kr/",{
-        method : "POST",
-        body : JSON.stringify({
-            ctl : "Product",
-            param1 : "webTest",
-        })
-    })
-    .then(response => response.text())
-    .then(result => responseCoupon(result))
-    .catch(error => console.log("error : " + error));
-}
+// const requestCoupon = () => {
+//     const responseCoupon = result => {
+//         result = JSON.parse(result);
+//         if(result.result == 1){
+//             couponInit(result.value);
+//         }
+//     }
+//     fetch("https://news.glserver.co.kr/",{
+//         method : "POST",
+//         body : JSON.stringify({
+//             ctl : "Product",
+//             param1 : "webTest",
+//         })
+//     })
+//     .then(response => response.text())
+//     .then(result => responseCoupon(result))
+//     .catch(error => console.log("error : " + error));
+// }
